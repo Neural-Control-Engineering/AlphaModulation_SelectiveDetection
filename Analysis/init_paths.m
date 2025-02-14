e63652ad8f43dbbc7e21b1c01d18fa9bb3efe0c0
kelley92 <kelley92@bu.edu>
2025-02-14 10:41:21 -0500

    fixed typo

--- Analysis/init_paths.m
+++ Analysis/init_paths.m
@@ -1,5 +1,5 @@
-raw_path = '/insomnia011/depts/neuralctrl/projects/nCORTEx/Project_Selective-Attention/Experiments/SELECT_DETECT/Data/RAW/';
+raw_path = '/insomnia001/depts/neuralctrl/projects/nCORTEx/Project_Selective-Attention/Experiments/SELECT_DETECT/Data/RAW/';
 ext_path = '/insomnia001/depts/neuralctrl/projects/nCORTEx/Project_Selective-Attention/Experiments/SELECT_DETECT/Data/EXT/';
 ftr_path = '/insomnia001/depts/neuralctrl/projects/nCORTEx/Project_Selective-Attention/Experiments/SELECT_DETECT/Data/FTR/';
-slrt_path = '/insomnia011/depts/neuralctrl/projects/nCORTEx/Project_Selective-Attention/Experiments/SELECT_DETECT/Data/RAW/SLRT/';
-subject_path = '/insomnia011/depts/neuralctrl/projects/nCORTEx/Project_Selective-Attention/Subjects/';
+slrt_path = '/insomnia001/depts/neuralctrl/projects/nCORTEx/Project_Selective-Attention/Experiments/SELECT_DETECT/Data/RAW/SLRT/';
+subject_path = '/insomnia001/depts/neuralctrl/projects/nCORTEx/Project_Selective-Attention/Subjects/';
