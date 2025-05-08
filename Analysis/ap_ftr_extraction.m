1e23724b1a6779238e9568870159cb19d612c959
kelley92 <kelley92@bu.edu>
2025-05-08 11:01:53 -0400

    ap ftr extraction

--- Analysis/ap_ftr_extraction.m
+++ Analysis/ap_ftr_extraction.m
@@ -1,26 +1,33 @@
 addpath(genpath('./'))
 addpath(genpath('~/n-CORTEx/'))
 addpath(genpath('~/circstat-matlab/'))
-sessionIDs 
-init_paths 
+sessionIDs; 
+init_paths; 
 
-load(strcat(subject_path, '3738-20240702/regionMap.mat'))
-
-ftr_file = strcat(ftr_path, 'AP/subj--3387-20240702_geno--Dbh-Cre-x-Gq-DREADD_npxls--R-npx10_phase--phase3_g0.mat');
+load(strcat(subject_path, '3387-20240121/regionMap_adjusted.mat'))
+ftr_file = strcat(ftr_path, 'AP/subj--3387-20240702_geno--Dbh-Cre-x-Gq-DREADD_npxls--R-npx10_phase--phase3_adjusted.mat');
 extToFtrAP(ext_path, expert_3387_session_ids, regMap, ftr_file)
 
-ftr_file = strcat(ftr_path, 'AP/subj--3738-20240702_geno--Dbh-Cre-x-Gq-DREADD_npxls--R-npx10_phase--phase3_g0.mat');
+load(strcat(subject_path, '3738-20240702/regionMap_adjusted.mat'))
+ftr_file = strcat(ftr_path, 'AP/subj--3738-20240702_geno--Dbh-Cre-x-Gq-DREADD_npxls--R-npx10_phase--phase3_adjusted.mat');
 extToFtrAP(ext_path, expert_3738_session_ids_v2, regMap, ftr_file)
 
 load(strcat(subject_path, '3755-20240828/regionMap.mat'))
-
-% ftr_file = strcat(ftr_path, 'AP/subj--3755-20240828_geno--Dbh-Cre-x-Gq-DREADD_npxls--R-npx10_phase--phase5_g0.mat');
-% extToFtrAP(ext_path, phase5_3755_expert_session_ids, regMap, ftr_file)
-
 ftr_file = strcat(ftr_path, 'AP/subj--3755-20240828_geno--Dbh-Cre-x-Gq-DREADD_npxls--R-npx10_phase--phase3_g0.mat');
 extToFtrAP(ext_path, expert_3755_session_ids, regMap, ftr_file)
 
 load(strcat(subject_path, '1075-20241202/regionMap.mat'))
-
 ftr_file = strcat(ftr_path, 'AP/subj--1075-20241202_geno--Wt_npxls--R-npx10_phase--phase3_g0.mat');
 extToFtrAP(ext_path, expert_1075_session_ids, regMap, ftr_file)
+
+% load(strcat(subject_path, '3738-20240702/regionMap.mat'))
+
+% ftr_file = strcat(ftr_path, 'AP/subj--3387-20240702_geno--Dbh-Cre-x-Gq-DREADD_npxls--R-npx10_phase--phase3_g0.mat');
+% extToFtrAP(ext_path, expert_3387_session_ids, regMap, ftr_file)
+
+% ftr_file = strcat(ftr_path, 'AP/subj--3738-20240702_geno--Dbh-Cre-x-Gq-DREADD_npxls--R-npx10_phase--phase3_g0.mat');
+% extToFtrAP(ext_path, expert_3738_session_ids_v2, regMap, ftr_file)
+
+
+% ftr_file = strcat(ftr_path, 'AP/subj--3755-20240828_geno--Dbh-Cre-x-Gq-DREADD_npxls--R-npx10_phase--phase5_g0.mat');
+% extToFtrAP(ext_path, phase5_3755_expert_session_ids, regMap, ftr_file)
