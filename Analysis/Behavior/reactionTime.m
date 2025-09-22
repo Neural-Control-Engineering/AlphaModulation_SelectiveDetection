665201eadfa48cac72d384a121b795831439e5c4
kelley92 <kelley92@bu.edu>
2025-09-22 14:33:40 -0400

    update reaction time script

--- Analysis/Behavior/reactionTime.m
+++ Analysis/Behavior/reactionTime.m
@@ -1,6 +1,6 @@
 function out = reactionTime(slrt_data)
     rts = cell(size(slrt_data,1),1);
-    t = linspace(-3,5,8001);
+    t = linspace(-3.5,5,8501);
     for i = 1:size(slrt_data,1)
         if strcmp(slrt_data(i,:).categorical_outcome{1}, 'Hit') || strcmp(slrt_data(i,:).categorical_outcome{1}, 'FA')
             stim_ind = slrt_data(i,:).left_trigger;
@@ -8,12 +8,14 @@ function out = reactionTime(slrt_data)
                 stim_ind = slrt_data(i,:).right_trigger;
             end
             try
-                react_inds = find(slrt_data(i,:).lick_detector{1}(stim_ind:stim_ind+5000)==1);
+                zero_ind = find(abs(linspace(-3.5,5,8501)) == min(abs(linspace(-3.5,5,8501))));
+                zero_ind = zero_ind;
+                react_inds = find(slrt_data(i,:).lick_detector{1}(stim_ind:stim_ind+(8501-zero_ind))==1);
             catch
                 react_inds = find(slrt_data(i,:).lick_detector{1}(stim_ind:end)==1);
             end
             try
-                rts{i} = t(react_inds(1)+3000);
+                rts{i} = t(react_inds(1)+zero_ind);
             catch 
                 rts{i} = nan;
             end
